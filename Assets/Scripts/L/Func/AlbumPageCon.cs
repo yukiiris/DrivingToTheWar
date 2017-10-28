a35fc045e69ea6bd90a04a1cef1af4c454a76ffc
LLH <837233905@qq.com>
2017-10-28 12:02:47 +0800

    lot

---
+++ Assets/Scripts/L/Func/AlbumPageCon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbumPageCon : MonoBehaviour {
+	public GameObject album;
+	public bool ifLast = true;
+	private int i = -1;
+
+	void Start () {
+		if(!ifLast){
+			i = 1;
+		}	
+		
+	}
+
+	void OnMouseDown () {
+		if((album.GetComponent<AlbumController>().page>0&&ifLast)||(album.GetComponent<AlbumController>().page < (album.transform.childCount-1)&&(!ifLast))){
+			album.GetComponent<AlbumController>().page += i;
+			album.GetComponent<AlbumController> ().ills [album.GetComponent<AlbumController> ().page-i].SetActive (false);
+
+		}
+	}
+}
