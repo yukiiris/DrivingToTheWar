bd52f3655e4f97b6d5bdf0a9be9853ef67b172b8
LLH <837233905@qq.com>
2017-11-04 10:44:33 +0800

    list and cover

---
+++ Assets/Scripts/L/Show/MenuButtonEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonEffect : MonoBehaviour {
+	public GameObject shade;
+	// Use this for initialization
+	void OnMouseEnter () {
+		shade.SetActive (true);
+	}
+
+	void OnMouseExit(){
+		shade.SetActive (false);
+	}
+
+}
