7dd48d17d4633c40a53394f5b288f458e87530cc
LLH <837233905@qq.com>
2017-11-13 16:05:22 +0800

    3d

--- Assets/FPSCon.cs
+++ Assets/FPSCon.cs
@@ -6,18 +6,22 @@ namespace UnityStandardAssets.Characters.FirstPerson{
 public class FPSCon : MonoBehaviour {
 
 		private MouseLook mouselook = new MouseLook();
-
+		public bool mouselock = true;
 
 
 	// Use this for initialization
 	void Start () {
 			mouselook.Init (transform, transform.GetChild (0));
-			mouselook.SetCursorLock (false);
+			mouselook.SetCursorLock (mouselock);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 			mouselook.LookRotation (transform, transform.GetChild (0));
+			mouselook.SetCursorLock (mouselock);
+			if(!mouselock){
+				this.enabled = false;
+			}
 	}
 	}
 }
