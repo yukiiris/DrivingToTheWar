dba206bc6cc7cef92fea834f669e907bc4b1b317
moltres <25852469@qq.com>
2017-12-15 15:26:50 +0800

    镜头调整

--- Assets/Scripts/B/Interact/Bug.cs
+++ Assets/Scripts/B/Interact/Bug.cs
@@ -6,30 +6,38 @@ public class Bug : MonoBehaviour {
 
 	public float x;
 	float preX;
+	bool isOut = false;
 
 	// Use this for initialization
 	void Start () {
 		preX = transform.position.x;
+		print(preX);
 		StartCoroutine(moveOut());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isOut)
+		{
+			StopCoroutine(moveOut());
+			StartCoroutine(moveIn());
+			isOut = false;
+		}
 	}
 
 	private IEnumerator moveOut()
 	{
-		while (Mathf.Abs(transform.position.x - x) > 0.1)
+		while (Mathf.Abs(transform.position.x - x) > 1)
 		{
 			transform.position = new Vector3(transform.position.x - 0.3f, transform.position.y, transform.position.z);
 			yield return 0.01f;
 		}
+		isOut = true;
 	}
 
 	private IEnumerator moveIn()
 	{
-		while (Mathf.Abs(transform.position.x - preX) > 0.1)
+		while (Mathf.Abs(transform.position.x - preX) > 1)
 		{
 			transform.position = new Vector3(transform.position.x + 0.3f, transform.position.y, transform.position.z);
 			yield return 0.01f;
---
+++ Assets/Scripts/B/Interact/Grain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grain : MonoBehaviour {
+	public Camera ca;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnMouseDown()
+	{
+		StartCoroutine(rotate());
+	}
+
+	private IEnumerator rotate()
+	{
+		float i = ca.transform.eulerAngles.x > 180 ? ca.transform.eulerAngles.x - 360 : ca.transform.eulerAngles.x;
+		while ((Mathf.Abs(i) > 0.1))
+		{
+			ca.transform.Rotate((Mathf.Abs(i) / i) * 0.1f, 0, 0);
+			yield return 0.1f;
+		}
+	}
+}
