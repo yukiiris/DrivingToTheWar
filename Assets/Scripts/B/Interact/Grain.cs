7a3a8a82cad299dbc284057e2814d633f6420181
moltres <25852469@qq.com>
2017-12-15 20:35:08 +0800

    失去了写代码的兴趣

--- Assets/Scripts/B/Interact/Grain.cs
+++ Assets/Scripts/B/Interact/Grain.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Grain : MonoBehaviour {
 	public Camera ca;
+	public GameObject game;
+	bool isEnd = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +14,11 @@ public class Grain : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isEnd)
+		{
+			game.SetActive(true);
+			isEnd = false;
+		}
 	}
 
 	private void OnMouseDown()
@@ -24,8 +31,10 @@ public class Grain : MonoBehaviour {
 		float i = ca.transform.eulerAngles.x > 180 ? ca.transform.eulerAngles.x - 360 : ca.transform.eulerAngles.x;
 		while ((Mathf.Abs(i) > 0.1))
 		{
-			ca.transform.Rotate((Mathf.Abs(i) / i) * 0.1f, 0, 0);
+			ca.transform.Rotate(0, -(Mathf.Abs(i) / i) * 0.1f, 0);
+			i = ca.transform.eulerAngles.x > 180 ? ca.transform.eulerAngles.x - 360 : ca.transform.eulerAngles.x;
 			yield return 0.1f;
 		}
+		isEnd = true;
 	}
 }
