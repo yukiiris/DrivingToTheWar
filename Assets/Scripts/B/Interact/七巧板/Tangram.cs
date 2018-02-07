93370323249ef75a4687db0e234bbcac9e9d63c1
moltres <25852469@qq.com>
2018-02-07 20:24:20 +0800

    七巧板功能实现一

---
+++ Assets/Scripts/B/Interact/七巧板/Tangram.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tangram : MonoBehaviour {
+	public TangramManager tangramManager;
+	public float x;
+	public float y;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(transform.position.x - x) < 1 && Mathf.Abs(transform.position.y - y) < 1)
+		{
+			transform.position = new Vector3(x, y, transform.position.z);
+			print(transform.position.x);
+			tangramManager.a++;
+			GetComponent<Drag>().enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/B/Interact/七巧板/TangramManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangramManager : MonoBehaviour {
+	public int a = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (a == 7)
+		{
+		}
+	}
+}
