4eb667a6a7b43842af4b9f64f40173169e033db8
moltres <25852469@qq.com>
2018-03-22 14:31:38 +0800

    七巧板

--- Assets/Scripts/B/Interact/七巧板/Tangram.cs
+++ Assets/Scripts/B/Interact/七巧板/Tangram.cs
@@ -8,6 +8,7 @@ public class Tangram : MonoBehaviour {
 	public float[] y = new float[4];
 	public int[] angels = new int[4];
 	public int angel = 1;
+	bool flag = true;
 
 	public GameObject arrow;
 	// Use this for initialization
@@ -17,17 +18,12 @@ public class Tangram : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Mathf.Abs(transform.position.x - x[tangramManager.n]) < 1 && Mathf.Abs(transform.position.y - y[tangramManager.n]) < 1)
+		if (Mathf.Abs(transform.position.x - x[tangramManager.n]) < 0.1 && Mathf.Abs(transform.position.y - y[tangramManager.n]) < 0.1 && flag)
 		{
 			transform.position = new Vector3(x[tangramManager.n], y[tangramManager.n], transform.position.z);
-			print(transform.position.x);
 			tangramManager.a++;
 			GetComponent<Drag>().enabled = false;
+			flag = false;
 		}
-	}
-
-	private void OnMouseDown()
-	{
-		arrow.SetActive(true);
 	}
 }
--- Assets/Scripts/B/Interact/七巧板/TangramManager.cs
+++ Assets/Scripts/B/Interact/七巧板/TangramManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TangramManager : MonoBehaviour {
 	public int a = 0;
 	public int n = 0;
+	public GameObject[] puzzle;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,8 +13,16 @@ public class TangramManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (a == 7)
+		if (a == 7 && n <= 3)
 		{
+			puzzle[n].SetActive(false);
+			n++;
+			puzzle[n].SetActive(true);
+			a = 0;
+		}
+		if (n == 4)
+		{
+			puzzle[n].SetActive(false);
 		}
 	}
 }
