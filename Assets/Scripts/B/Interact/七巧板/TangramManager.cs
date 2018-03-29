996a3c048ae973b44f2853ee84a2818e2377473c
moltres <25852469@qq.com>
2018-03-29 20:25:15 +0800

    1

--- Assets/Scripts/B/Interact/七巧板/TangramManager.cs
+++ Assets/Scripts/B/Interact/七巧板/TangramManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TangramManager : MonoBehaviour {
 	public int a = 0;
-	public int n = 0;
+	public int n = 1;
 	public GameObject[] puzzle;
 	// Use this for initialization
 	void Start () {
